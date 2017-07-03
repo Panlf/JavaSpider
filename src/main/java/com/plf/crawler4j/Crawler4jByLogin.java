8ad0bcf9e4f9479c8eb40d343a603819ebafb624
Panlf <liangfeng_pan@163.com>
2017-07-03 21:03:32 +0800

    edit sth

--- src/main/java/com/plf/crawler4j/Crawler4jByLogin.java
+++ src/main/java/com/plf/crawler4j/Crawler4jByLogin.java
@@ -1,10 +1,12 @@
-package cm.plf.crawler4j;
+package com.plf.crawler4j;
+
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
 
 /**
  * 将推酷、掘金、开发者头条中的收藏存入数据库
  * @author Panlf 2017年6月30日下午10:15:55
  *
  */
-public class Crawler4jExample {
+public class Crawler4jByLogin extends WebCrawler {
 
 }
---
+++ src/main/java/com/plf/crawler4j/Crawler4jExample.java
@@ -0,0 +1,12 @@
+package com.plf.crawler4j;
+
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+
+/**
+ * Crawler4j官方案例
+ * @author Panlf 2017年7月3日下午8:44:14
+ *
+ */
+public class Crawler4jExample extends WebCrawler {
+
+}
