529379b5d83d0a0ec8649cbc7b9a8ca6878b5d56
Panlf <liangfeng_pan@163.com>
2017-08-26 23:52:23 +0800

    add analy url

--- src/main/java/com/plf/httpclient/HttpClientExample.java
+++ src/main/java/com/plf/httpclient/HttpClientExample.java
@@ -1,6 +1,12 @@
 package com.plf.httpclient;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -94,5 +100,30 @@ public class HttpClientExample {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	@Test
+	public void analyzeURL(){
+		String path="http://www.tuicool.com/articles/zQNfyaN";
+		try {
+			URL url=new URL(path);
+			URLConnection conn=url.openConnection();
+			conn.connect();
+			InputStream ins=conn.getInputStream();
+			InputStreamReader insread=new InputStreamReader(ins,"utf-8");
+			BufferedReader buff=new BufferedReader(insread);
+			String nextline=buff.readLine();
+			while(nextline != null){
+				System.out.println(nextline);
+				nextline=buff.readLine();
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
